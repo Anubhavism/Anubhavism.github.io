336232d125f7deaed7e2f4972d824cca5a82c57e
Anubhav <anubhavism94@gmail.com>
2017-05-24 16:08:49 +0530

    First commit

--- scripts/app.js
+++ scripts/app.js
@@ -1,24 +1,31 @@
-'use strict';
+var app=angular.module('myApp',['infinite-scroll']);
+app.controller('mainCtrl',function($scope,$http){
+	$http.get('static-data/profileDetails.json').then(function(response){
+		$scope.userInfo=response.data;
+	});
+	$http.get('static-data/smallFeed.json').then(function(response){
+		$scope.smallFeed=response.data;
+		$scope.temp=response.data;
+		
+	});
+	$http.get('static-data/userDetails.json').then(function(response){
+		$scope.newsFeed=response.data;
+		$scope.tempFeed=response.data;
+	});
+	$scope.contacts=['Aparajita Tiwari','Tanushree Mahto','Bhawna Rai','Hemant Rai','Alafazam Khan','Parmender Chahal','Akshay Kumar'];
+	 $scope.loadMore = function() {
+        for (let i = 0; i < 5; i++) {
+            $scope.smallFeed[$scope.smallFeed.length]=$scope.temp[i];
+            
+        }
+    };
+    $scope.loadFeed=function(){
+    	for(let i=0;i<3;i++){
+    		$scope.newsFeed[$scope.newsFeed.length]=$scope.tempFeed[i];
+    	}
+    };
+   
+});
 
-var myApp= angular.module('fino',['ngRoute','lumx']);
 
-myApp.config(function($routeProvider){
-	$routeProvider
-		.when('/',{
-			templateUrl: 'views/front.html',
-			controller: 'MainCtrl'
-		})
-		.when('/quiz',{
-			templateUrl: 'views/quiz.html',
-			controller: 'QuizCtrl'
-		})
-		.when('/result',{
-			templateUrl: 'views/result.html',
-			controller: 'ResultCtrl'
-		})
-		.otherwise({
-			redirectTo: '/'
-		});
-}).run(['$rootScope',  function() {}]);
-    
 
